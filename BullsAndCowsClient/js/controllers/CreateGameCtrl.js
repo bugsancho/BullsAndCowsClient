1cce413737978de1a507778445a043fd1c9bf429
bugsancho <bugsancho@gmail.com>
2014-09-28 00:34:21 +0300

    Minor bug fixes

--- BullsAndCowsClient/js/controllers/CreateGameCtrl.js
+++ BullsAndCowsClient/js/controllers/CreateGameCtrl.js
@@ -7,10 +7,11 @@ app.controller('CreateGameCtrl', ['$scope', 'notifier', 'createGame', 'notificat
 
     function createGameFunc(game) {
         createGame.createGame(game).then(function (data) {
-                notifier.success('Game created successfully!');
+            notifier.success('Game created successfully!');
+            window.location = '#/games';
             },
             function (error) {
-                if (error.status == '401') {
+                if (error.status === '401') {
                     notifier.error('You must be logged in to create a game!');
                 }
                 else if (error.data.Message) {
